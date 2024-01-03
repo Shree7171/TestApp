bde3e972cec96caf7e2a1fb7d9cda6726a7b330c
Shree7171 <shreekumbhar7171@gmail.com>
2024-01-03 13:35:48 +0530

    this is new commit

---
+++ app/src/main/java/com/example/project65/Second.java
@@ -0,0 +1,15 @@
+package com.example.project65;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Second extends Activity {
+    Button button;
+    @Override
+    public void onCreate(Bundle savedInstnceState) {
+
+        super.onCreate(savedInstnceState);
+        setContentView(R.layout.activity_main);
+    }
+}
