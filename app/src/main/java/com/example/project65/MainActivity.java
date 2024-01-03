0c87216448d2e3fd96d4e94019d6c614dd74c3fe
Shree7171 <shreekumbhar7171@gmail.com>
2024-01-03 14:13:33 +0530

    this is third commit

--- app/src/main/java/com/example/project65/MainActivity.java
+++ app/src/main/java/com/example/project65/MainActivity.java
@@ -25,7 +25,7 @@ Button button;
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this,Second.class);
+                    Intent intent =new Intent(MainActivity.this,Second.class);
 
                     startActivity(intent);
                 }
